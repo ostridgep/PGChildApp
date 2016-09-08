a448dc39b1b770c070498d417dff77f68ae070e2
ostridgep <paul@ostridge.com>
2016-09-08 17:47:59 +0100

    xx

--- PGChildApp/WebContent/js/index.js
+++ PGChildApp/WebContent/js/index.js
@@ -56,13 +56,14 @@ var app = {
     bindEvents: function() {
     	
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        checkPassedParams();
+        
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	checkPassedParams();
         app.receivedEvent('deviceready');
        
     },
